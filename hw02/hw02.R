827533e092127ddc944a9a65d8e6d1423c475207
Juan Fercovic <juan.fercovic@ubc.ca>
2019-09-25 09:37:00 -0700

    hw2

--- hw02/hw02.R
+++ hw02/hw02.R
@@ -27,9 +27,43 @@ max_gdp <-
   summarize(max_gdp_country = max(gdpPercap, na.rm = TRUE)) %>% 
 
 
+#1.4b six rows: the rows with the three largest GDP per capita, and the rows with the three smallest GDP per capita
 
 
-  
-  
+#1.5 scatterplot of Canada’s life expectancy vs. GDP per capita using ggplot2
+  canada <- filter(gapminder, country %in% c("Canada"))
+ggplot(data=canada,aes(x=log(gdpPercap),y=lifeExp)) + geom_point()  
+
+
+
+#Exercise 2: Explore individual variables with dplyr
+
+
+#I will use Gapminder, categorical variable country and numerical life expentancy
+ex2 <- select(gapminder,year,country,lifeExp)
+print(ex2)
+
+#What are possible values (or range, whichever is appropriate) of each variable?
+ex2 <- select(gapminder,year,country,lifeExp) %>%
+dplyr::select(country)
+
+#What values are typical? What’s the spread? What’s the distribution? Etc., tailored to the variable at hand.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   
