a39f097285c5361f5012e9b851b559f7d8a1beb5
Juan Fercovic <juan.fercovic@ubc.ca>
2019-09-27 14:30:24 -0700

    update

--- hw02/hw02.R
+++ hw02/hw02.R
@@ -1,9 +1,14 @@
 #Assignment 2
 install.packages("gapminder")
-library(gapminder)
 install.packages("dplyr")    # alternative installation of the %>%
 install.packages('tidyverse')
+install.packages("magrittr")
+install.packages("qwraps2")
+library(gapminder)
 library(tidyverse)
+library(dplyr)
+library(magrittr)
+library(qwraps2)
 gapminder
 
 #1.1
@@ -37,42 +42,45 @@ ggplot(data=canada,aes(x=log(gdpPercap),y=lifeExp)) + geom_point()
 
 
 #Exercise 2: Explore individual variables with dplyr
-
+#What values are typical? What’s the spread? What’s the distribution? Etc., tailored to the variable at hand.
 
 #I will use Gapminder, categorical variable country and numerical life expentancy
 ex2 <- select(gapminder,year,country,lifeExp)
 
 print(ex2)
 
-summarize(ex2, lifeExp)
-
-#Examples
-gapminder %>% 
-  filter(year == 2007) %>% 
-  summarise(meanlife = mean(lifeExp))
-
-#generate summary statistics for each value of another variable we use the group_by() function
-gapminder %>% 
-  filter(year == 2007) %>% 
-  group_by(continent) %>% 
-  summarise(meanlife = mean(lifeExp))
-
-#standard error of the life expectancy per continent:
-gapminder %>%
-  filter(year == 2002) %>%	
-  group_by(continent) %>%
-  summarize(se_pop = sd(lifeExp)/sqrt(n()))
-
-#number of countries included in the dataset for the year 2002
+#number of countries by continent included in the dataset (for the year 2002)
 gapminder %>%
   filter(year == 2002) %>%
   count(continent, sort = TRUE)
 
-#What are possible values (or range, whichever is appropriate) of each variable?
-ex2 <- select(gapminder,year,country,lifeExp) %>%
-dplyr::select(country)
 
-#What values are typical? What’s the spread? What’s the distribution? Etc., tailored to the variable at hand.
+#Years in the sample
+gapminder %>%
+  count(year, sort = TRUE)
+
+
+#summary table for Life Exp.
+args(summary_table)
+
+summary_lifeexp <-
+  list("Life Expentancy" =
+         list("min" = ~ min(gapminder$lifeExp),
+              "max" = ~ max(gapminder$lifeExp),
+              "mean" = ~ mean(gapminder$lifeExp),
+              "sd" = ~ sd(gapminder$lifeExp)),
+       "Years" = 
+         list("min" = ~ min(gapminder$year),
+              "max" = ~ max(gapminder$year),
+              "N" = ~ add_count(gapminder$year,)))
+
+table <- summary_table(gapminder, summary_lifeexp)
+
+#Overall summary
+table
+#Group by Continent
+table_cont <- summary_table(dplyr::group_by(gapminder, continent), summary_lifeexp)
+
 
 
 #Exercise 3: Explore various plot types
